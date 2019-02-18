5911847c5d757d5afd4c5337ef9bb775f40cd5c9
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-02-18 22:39:44 +0800

    Changes: 1.	Created a copy of the while loop in order to experiment with some stuff
    2.	Removed the for loop to extract out all the subjects, as the assumption
    	is that only the driver's face, or the closest face should be the one
    	used for prediction only.
    
    3.	Simplified the calculation of the Eye Aspect Ratio into one line
    
    4.	Started working on seperating the different operations out into seperate
    	functions in order to run them as seperate processes in order to take
    	advantage of the multicore CPU on the system.

--- Drowsiness_Detection.py
+++ Drowsiness_Detection.py
@@ -5,6 +5,8 @@ import dlib
 import cv2
 
 # Define the function for calculating eye aspect ratio with eucilidean distance
+
+
 def eye_aspect_ratio(eye):
     # Calculate the distance between the 3 pairs of point.
     A = distance.euclidean(eye[1], eye[5])
@@ -14,13 +16,14 @@ def eye_aspect_ratio(eye):
     return ((A + B) / (2.0 * C))
 
 
-thresh = 0.22 # Threshold value for the Eye Aspect Ratio.
-count = 0 # Global variable used to keep track of the consecutive number of times the 'EAR' is below threshold
+thresh = 0.22  # Threshold value for the Eye Aspect Ratio.
+count = 0  # Global variable used to keep track of the consecutive number of times the 'EAR' is below threshold
 
 # Get a function from dlib to be used to detect faces, or 'subjects'
 detect = dlib.get_frontal_face_detector()
 # Get the .dat file that stores the prediction model used by dlib, and pass it into the function to get a model out.
-predict_data = dlib.shape_predictor(r"C:\Users\user\Documents\Projects\Drowsiness_Detection\shape_predictor_68_face_landmarks.dat")
+predict_data = dlib.shape_predictor(
+    r"C:\Users\user\Documents\Projects\Drowsiness_Detection\shape_predictor_68_face_landmarks.dat")
 
 # Give names to the detected features, 'eyes'
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
@@ -48,60 +51,119 @@ cap = cv2.VideoCapture(0)
 
 """
 
+# Function to run in child process that deals with just reading the face and not predicting
+def get_face():
+    # Read and store the newly captured image
+    ret, frame = cap.read()
+    # Redraw the frame if neccessary to capture less data which means less processing power needed.
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Detect faces, after applying the filter for color recognition
+    subjects = detect(gray, 0)
+
+    try:
+        # Try to get the 'first face' out, the closet face to the camera
+        # if face detected, put the face into the pipe
+        subject = subjects[0]
+    except:
+        # Notify user that face not detected
+        continue
+
 
 
 # Infinite loop to read frames from the video output and put into a queue
 while True:
-    # Read and store the newly captured image
-    ret, frame = cap.read()
+    # Read the pipe, do the below only if image avail in the pipe
 
-while True:
-    # Read and store the newly captured image
-    ret, frame = cap.read()
-    # Redraw the frame if neccessary to capture less data which means less processing power needed.
-    # frame = imutils.resize(frame, width=450) # is this needed?
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Detect faces, after applying the filter for color recognition
-    subjects = detect(gray, 0)
-    # Loop through all the faces detected, if there is more than one
-    for subject in subjects:
-        shape = predict_data(gray, subject)
-        # Convert the shape data to a NumPy Array
-        shape = face_utils.shape_to_np(shape)
-        leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
+    shape = predict_data(gray, subject)
+    # Convert the shape data to a NumPy Array
+    shape = face_utils.shape_to_np(shape)
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
 
-        # Calculate the EAR for both eyes and store them, to calculate the average EAR
-        leftEAR = eye_aspect_ratio(leftEye)
-        rightEAR = eye_aspect_ratio(rightEye)
-        ear = (leftEAR + rightEAR) / 2.0
+    ear = (eye_aspect_ratio(leftEye) + eye_aspect_ratio(rightEye)) / 2.0
 
-        # Draw on the frame so that the user can see the eye tracking in real time.
-        # Create the contours out before drawing them.
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        # To draw out the contours around the eyes
-        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
-        print(f"EAR: {ear}")
-        if ear < thresh:
-            count += 1
-            # Number of frames where EAR is below threshold before counted as falling asleep
-            if count >= 3:
-                # Alert the user by putting text onto the frame directly.
-                cv2.putText(frame, "**********************ALERT!**********************", (20, 30),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                cv2.putText(frame, "**********************ALERT!**********************", (20, 460),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                # Alert the user by sounding the alarm.
-                # Pass the event via the data
-                # alarm()
-        else:
-            count = 0 # Reset the count
+
+    # Draw on the frame so that the user can see the eye tracking in real time.
+    # Create the contours out before drawing them.
+    leftEyeHull = cv2.convexHull(leftEye)
+    rightEyeHull = cv2.convexHull(rightEye)
+    # To draw out the contours around the eyes
+    cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+    cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+    print(f"EAR: {ear}")
+    if ear < thresh:
+        count += 1
+        # Number of frames where EAR is below threshold before counted as falling asleep
+        if count >= 3:
+            # Alert the user by putting text onto the frame directly.
+            cv2.putText(frame, "**********************ALERT!**********************", (20, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(frame, "**********************ALERT!**********************", (20, 460),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            # Alert the user by sounding the alarm.
+            # Pass the event via the data
+            # alarm()
+    else:
+        count = 0  # Reset the count
+
     cv2.imshow("Frame", frame)
 
     if (cv2.waitKey(1) & 0xFF) == ord("q"):
         break
 
 cv2.destroyAllWindows()
+
+
+
+# while True:
+#     # Read and store the newly captured image
+#     ret, frame = cap.read()
+#     # Redraw the frame if neccessary to capture less data which means less processing power needed.
+#     # frame = imutils.resize(frame, width=450) # is this needed?
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     # Detect faces, after applying the filter for color recognition
+#     subjects = detect(gray, 0)
+#     # Loop through all the faces detected, if there is more than one
+#     for subject in subjects:
+#         shape = predict_data(gray, subject)
+#         # Convert the shape data to a NumPy Array
+#         shape = face_utils.shape_to_np(shape)
+#         leftEye = shape[lStart:lEnd]
+#         rightEye = shape[rStart:rEnd]
+
+#         # Calculate the EAR for both eyes and store them, to calculate the average EAR
+#         leftEAR = eye_aspect_ratio(leftEye)
+#         rightEAR = eye_aspect_ratio(rightEye)
+#         ear = (leftEAR + rightEAR) / 2.0
+
+#         # Draw on the frame so that the user can see the eye tracking in real time.
+#         # Create the contours out before drawing them.
+#         leftEyeHull = cv2.convexHull(leftEye)
+#         rightEyeHull = cv2.convexHull(rightEye)
+#         # To draw out the contours around the eyes
+#         cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+#         cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+#         print(f"EAR: {ear}")
+#         if ear < thresh:
+#             count += 1
+#             # Number of frames where EAR is below threshold before counted as falling asleep
+#             if count >= 3:
+#                 # Alert the user by putting text onto the frame directly.
+#                 cv2.putText(frame, "**********************ALERT!**********************", (20, 30),
+#                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+#                 cv2.putText(frame, "**********************ALERT!**********************", (20, 460),
+#                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+#                 # Alert the user by sounding the alarm.
+#                 # Pass the event via the data
+#                 # alarm()
+#         else:
+#             count = 0 # Reset the count
+#     cv2.imshow("Frame", frame)
+
+#     if (cv2.waitKey(1) & 0xFF) == ord("q"):
+#         break
+
+# cv2.destroyAllWindows()
